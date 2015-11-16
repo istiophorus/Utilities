27a2ca94ea63d02bb6cbcb83e217fa46cf6f435f
istiophorus <rskotak@gmail.com>
2015-11-16 18:42:39 +0100

    Added some unit tests.

---
+++ UtilitiesTests/ArrayToolsSubarrayTests.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Utilities.Tests
+{
+	[TestClass]
+	public sealed class ArrayToolsSubarrayTests
+	{
+		[TestMethod]
+		public void StaticTest1()
+		{
+			var d1 = new[]
+                {
+                    1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6
+                };
+
+			CollectionAssert.AreEqual(d1, ArrayTools.Subarray(d1, 0, d1.Length));
+		}
+
+		[TestMethod]
+		public void StaticTest2()
+		{
+			var d1 = new[]
+                {
+                    1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6
+                };
+
+			CollectionAssert.AreEqual(new[]
+                {
+                    1.0, 1.1, 1.2, 1.3, 1.4, 1.5
+                },
+							ArrayTools.Subarray(d1, 0, d1.Length - 1));
+		}
+
+		[TestMethod]
+		public void StaticTest3()
+		{
+			var d1 = new[]
+                {
+                    1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6
+                };
+
+			CollectionAssert.AreEqual(new[]
+                {
+                    1.1, 1.2, 1.3, 1.4, 1.5, 1.6
+                },
+							ArrayTools.Subarray(d1, 1, d1.Length - 1));
+		}
+
+		[TestMethod]
+		public void StaticTest4()
+		{
+			var d1 = new[]
+                {
+                    1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6
+                };
+
+			CollectionAssert.AreEqual(new[]
+                {
+                    1.2, 1.3, 1.4, 1.5, 1.6
+                },
+							ArrayTools.Subarray(d1, 2, d1.Length - 2));
+		}
+
+		[TestMethod]
+		public void StaticTest5()
+		{
+			var d1 = new[]
+                {
+                    1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6
+                };
+
+			CollectionAssert.AreEqual(new[]
+                {
+                    1.6
+                },
+							ArrayTools.Subarray(d1, d1.Length - 1, 1));
+		}
+
+		[TestMethod]
+		public void StaticTest6()
+		{
+			var d1 = new[]
+                {
+                    1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6
+                };
+
+			CollectionAssert.AreEqual(new[]
+                {
+                    1.5, 1.6
+                },
+							ArrayTools.Subarray(d1, d1.Length - 2, 2));
+		}
+
+		[TestMethod]
+		public void StaticTest7()
+		{
+			var d1 = new[]
+                {
+                    1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6
+                };
+
+			CollectionAssert.AreEqual(new Double[0],
+							ArrayTools.Subarray(d1, d1.Length, 0));
+		}
+
+		[TestMethod]
+		public void StaticTest8()
+		{
+			var d1 = new[]
+                {
+                    1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6
+                };
+
+			CollectionAssert.AreEqual(new Double[0],
+							ArrayTools.Subarray(d1, 0, 0));
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentOutOfRangeException))]
+		public void StaticTestError1()
+		{
+			var d1 = new[]
+                {
+                    1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6
+                };
+
+			ArrayTools.Subarray(d1, -1, d1.Length);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentOutOfRangeException))]
+		public void StaticTestError2()
+		{
+			var d1 = new[]
+                {
+                    1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6
+                };
+
+			ArrayTools.Subarray(d1, d1.Length + 1, 1);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentOutOfRangeException))]
+		public void StaticTestError3()
+		{
+			var d1 = new[]
+                {
+                    1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6
+                };
+
+			ArrayTools.Subarray(d1, 0, d1.Length + 1);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentOutOfRangeException))]
+		public void StaticTestError4()
+		{
+			var d1 = new[]
+                {
+                    1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6
+                };
+
+			ArrayTools.Subarray(d1, 0, -1);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentOutOfRangeException))]
+		public void StaticTestError5()
+		{
+			var d1 = new[]
+                {
+                    1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6
+                };
+
+			ArrayTools.Subarray(d1, 5, -1);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentOutOfRangeException))]
+		public void StaticTestError6()
+		{
+			var d1 = new[]
+                {
+                    1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6
+                };
+
+			ArrayTools.Subarray(d1, 1, d1.Length);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentOutOfRangeException))]
+		public void StaticTestError7()
+		{
+			var d1 = new[]
+                {
+                    1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6
+                };
+
+			ArrayTools.Subarray(d1, d1.Length, 1);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void StaticTestError8()
+		{
+			ArrayTools.Subarray<Double>(null, 0, 123);
+		}
+
+	}
+}
--- UtilitiesTests/BinaryEncoderTests.cs
+++ UtilitiesTests/BinaryEncoderTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -47,10 +48,6 @@ namespace Utilities.Tests
 
 		private readonly BinaryEncoder _encoder = new BinaryEncoder();
 
-		public BinaryEncoderTests()
-		{
-		}
-
 		[TestMethod]
 		public void ReadWriteTestInt32()
 		{
@@ -77,5 +74,315 @@ namespace Utilities.Tests
 				(w, x) => _encoder.WriteUInt64Encoded(w, x),
 				r => _encoder.ReadUInt64Decoded(r));
 		}
-	}
+
+		public BinaryEncoderTests()
+        {
+            _prepareValueForUInt32 = x =>
+                {
+                    _random.NextBytes(x);
+
+                    Int32 zeroCount = _random.Next() % x.Length;
+
+                    for (Int32 w = x.Length - zeroCount; w < x.Length; w++)
+                    {
+                        x[w] = 0;
+                    }
+
+                    return BitConverter.ToUInt32(x, 0);
+                };
+        }
+
+        private readonly Random _random = new Random(Environment.TickCount);
+
+        private void Test<T>(
+            Int32 testsCount, 
+            Func<Byte[], T> prepareValue, 
+            Func<BinaryWriter, T, Int32> serializationAction, 
+            Func<BinaryReader, T> deserializationAction) where T : struct, IComparable
+        {
+            var r = new Random(Environment.TickCount);
+
+            var buffer = new Byte[Marshal.SizeOf(typeof(T))];
+
+            for (Int32 q = 0; q < testsCount; q++)
+            {
+                T testValue = prepareValue(buffer);
+
+                SingleValueTest(serializationAction, deserializationAction, testValue);
+            }
+        }
+
+        private static void SingleValueTest<T>(
+            Func<BinaryWriter, T, Int32> serializationAction, 
+            Func<BinaryReader, T> deserializationAction, 
+            T testValue) where T : struct, IComparable
+        {
+            Byte[] bytes;
+
+            using (var ms = new MemoryStream())
+            {
+                Int32 bytesCount;
+
+                using (var writer = new BinaryWriter(ms))
+                {
+                    bytesCount = serializationAction(writer, testValue);
+                }
+
+                bytes = ms.ToArray();
+
+                Assert.AreEqual(bytesCount, bytes.Length, "Returned bytes count is different than buffer size " + bytesCount + " " + bytes.Length);
+            }
+
+            using (var ms = new MemoryStream(bytes))
+            {
+                using (var reader = new BinaryReader(ms))
+                {
+                    T value = deserializationAction(reader);
+
+                    Assert.AreEqual(testValue, value, "Values are different " + testValue + " " + value);
+                }
+            }
+        }
+
+        private const Int32 TestsCount = 100000;
+
+		[TestMethod]
+        public void TestRandomInt64()
+        {
+            Test<Int64>(
+                TestsCount, 
+                x =>
+                    {
+                        _random.NextBytes(x);
+
+                        Int32 zeroCount = _random.Next() % x.Length;
+
+                        for (Int32 w = x.Length - zeroCount; w < x.Length; w++)
+                        {
+                            x[w] = 0;
+                        }
+
+                        return BitConverter.ToInt64(x, 0);
+                    }, 
+                (w, v) => _encoder.WriteInt64Encoded(w, v), 
+                r => _encoder.ReadInt64Decoded(r));
+        }
+
+		[TestMethod]
+        public void TestRandomUInt64()
+        {
+            Test<UInt64>(
+                TestsCount, 
+                x =>
+                    {
+                        _random.NextBytes(x);
+
+                        Int32 zeroCount = _random.Next() % x.Length;
+
+                        for (Int32 w = x.Length - zeroCount; w < x.Length; w++)
+                        {
+                            x[w] = 0;
+                        }
+
+                        return BitConverter.ToUInt64(x, 0);
+                    }, 
+                (w, v) => _encoder.WriteUInt64Encoded(w, v), 
+                r => _encoder.ReadUInt64Decoded(r));
+        }
+
+		[TestMethod]
+        public void TestRandomInt32()
+        {
+            Test<Int32>(
+                TestsCount, 
+                x =>
+                    {
+                        _random.NextBytes(x);
+
+                        Int32 zeroCount = _random.Next() % x.Length;
+
+                        for (Int32 w = x.Length - zeroCount; w < x.Length; w++)
+                        {
+                            x[w] = 0;
+                        }
+
+                        return BitConverter.ToInt32(x, 0);
+                    }, 
+                (w, v) => _encoder.WriteInt32Encoded(w, v), 
+                r => _encoder.ReadInt32Decoded(r));
+        }
+
+        private readonly Func<Byte[], UInt32> _prepareValueForUInt32;
+
+		[TestMethod]
+        public void TestRandomUInt32()
+        {
+            Test<UInt32>(
+                TestsCount, 
+                _prepareValueForUInt32, 
+                (w, v) => _encoder.WriteUInt32Encoded(w, v), 
+                r => _encoder.ReadUInt32Decoded(r));
+        }
+
+		[TestMethod]
+        public void TestZero()
+        {
+            SingleValueAllTests(0);
+        }
+
+		[TestMethod]
+        public void TestMax()
+        {
+            SingleValueTest<Int32>(
+                (w, v) => _encoder.WriteInt32Encoded(w, v), 
+                r => _encoder.ReadInt32Decoded(r), 
+                Int32.MaxValue);
+
+            SingleValueTest<Int32>(
+                (w, v) => _encoder.WriteInt32Encoded(w, v), 
+                r => _encoder.ReadInt32Decoded(r), 
+                Int32.MinValue);
+
+            SingleValueTest<UInt32>(
+                (w, v) => _encoder.WriteUInt32Encoded(w, v), 
+                r => _encoder.ReadUInt32Decoded(r), 
+                UInt32.MaxValue);
+
+            SingleValueTest<UInt32>(
+                (w, v) => _encoder.WriteUInt32Encoded(w, v), 
+                r => _encoder.ReadUInt32Decoded(r), 
+                UInt32.MinValue);
+
+            SingleValueTest<Int64>(
+                (w, v) => _encoder.WriteInt64Encoded(w, v), 
+                r => _encoder.ReadInt64Decoded(r), 
+                0);
+
+            SingleValueTest<Int64>(
+                (w, v) => _encoder.WriteInt64Encoded(w, v), 
+                r => _encoder.ReadInt64Decoded(r), 
+                Int64.MaxValue);
+
+            SingleValueTest<Int64>(
+                (w, v) => _encoder.WriteInt64Encoded(w, v), 
+                r => _encoder.ReadInt64Decoded(r), 
+                Int64.MinValue);
+
+            SingleValueTest<UInt64>(
+                (w, v) => _encoder.WriteUInt64Encoded(w, v), 
+                r => _encoder.ReadUInt64Decoded(r), 
+                UInt64.MaxValue);
+
+            SingleValueTest<UInt64>(
+                (w, v) => _encoder.WriteUInt64Encoded(w, v), 
+                r => _encoder.ReadUInt64Decoded(r), 
+                UInt64.MinValue);
+        }
+
+		[TestMethod]
+        public void TestSeries()
+        {
+            for (Int32 q = 0; q < 10000000; q += 13)
+            {
+                Int64 testValue = q;
+
+                SingleValueAllTests(testValue);
+            }
+        }
+
+        private void SingleValueAllTests(Int64 testValue)
+        {
+            if (testValue <= Int32.MaxValue && testValue >= Int32.MinValue)
+            {
+                SingleValueTest<Int32>(
+                    (w, v) => _encoder.WriteInt32Encoded(w, v), 
+                    r => _encoder.ReadInt32Decoded(r), 
+                    (Int32)testValue);
+
+                SingleValueTest<UInt32>(
+                    (w, v) => _encoder.WriteUInt32Encoded(w, v), 
+                    r => _encoder.ReadUInt32Decoded(r), 
+                    (UInt32)testValue);
+            }
+
+            SingleValueTest<Int64>(
+                (w, v) => _encoder.WriteInt64Encoded(w, v), 
+                r => _encoder.ReadInt64Decoded(r), 
+                (Int64)testValue);
+
+            SingleValueTest<UInt64>(
+                (w, v) => _encoder.WriteUInt64Encoded(w, v), 
+                r => _encoder.ReadUInt64Decoded(r), 
+                (UInt64)testValue);
+        }
+
+		[TestMethod]
+        public void TestBorderValues()
+        {
+            SingleValueAllTests(-1);
+
+            SingleValueAllTests(0);
+
+            SingleValueAllTests(1);
+
+            SingleValueAllTests(0x80 - 1);
+
+            SingleValueAllTests(0x80);
+
+            SingleValueAllTests(0x4000 - 1);
+
+            SingleValueAllTests(0x4000);
+
+            SingleValueAllTests(0x200000 - 1);
+
+            SingleValueAllTests(0x200000);
+
+            SingleValueAllTests(0x10000000 - 1);
+
+            SingleValueAllTests(0x10000000);
+
+            SingleValueAllTests(0x0800000000 - 1);
+
+            SingleValueAllTests(0x0800000000);
+
+            SingleValueAllTests(0x040000000000 - 1);
+
+            SingleValueAllTests(0x040000000000);
+
+            SingleValueAllTests(0x02000000000000 - 1);
+
+            SingleValueAllTests(0x02000000000000);
+
+            SingleValueAllTests(0x0100000000000000 - 1);
+
+            SingleValueAllTests(0x0100000000000000);
+        }
+
+		[TestMethod]
+        public void TestBorderValuesRanges()
+        {
+            var ranges = new Int64[]
+                {
+                    0, 
+                    0x80, 
+                    0x4000, 
+                    0x200000, 
+                    0x10000000, 
+                    0x0800000000, 
+                    0x040000000000, 
+                    0x02000000000000, 
+                    0x0100000000000000
+                };
+
+            for (Int32 q = 0; q < ranges.Length; q++)
+            {
+                Int64 range = ranges[q];
+
+                for (Int64 w = range - 16; w < range + 16384; w++)
+                {
+                    SingleValueAllTests(w);
+                }
+            }
+        }
+ 	}
 }
